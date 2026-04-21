4d1339b861ca9f3ea4da7cf7743abab7b462acbc
Jan Torres <153082161+JanToro26@users.noreply.github.com>
2026-04-21 20:28:32 +0200

    Generating conflict

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        int values = 5;
+        int values = 7;
         for (int index = 0; index < values; index++) {
             System.out.println("First Commit");
         }
