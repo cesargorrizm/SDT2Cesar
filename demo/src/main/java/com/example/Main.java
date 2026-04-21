010c1f52959d5f2d7ec133925de524f3b253c4f9
Cesar <cesar.gorriz@alu.uclm.es>
2026-04-21 18:34:42 +0200

    Second Commit

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -3,5 +3,10 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        int values = 5;
+        for (int index = 0; index < values; index++) {
+            System.out.println("First Commit");
+        }
     }
 }
